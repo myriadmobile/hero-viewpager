fd97bf92c51aad25336a5ebcc402490a640373be
Adam Koski <akoski@myriadmobile.com>
2014-08-20 12:22:27 -0500

    Expanded the Example app to include a basic impl and a springboard activity. Updated readme with basic usage help.

--- Example/src/main/java/com/myriadmobile/library/heroviewpager/example/BurnsHeroExampleActivity.java
+++ Example/src/main/java/com/myriadmobile/library/heroviewpager/example/BurnsHeroExampleActivity.java
@@ -24,36 +24,38 @@
 
 package com.myriadmobile.library.heroviewpager.example;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
+import com.myriadmobile.library.heroviewpager.BurnsHeroActivity;
+import com.myriadmobile.library.heroviewpager.HeroPagerAdapter;
 
 /**
  *
  */
-public class MainActivity extends Activity {
+public class BurnsHeroExampleActivity extends BurnsHeroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button_example_basic).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), BasicHeroExampleActivity.class));
-            }
-        });
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        findViewById(R.id.button_example_burns).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), BurnsHeroExampleActivity.class));
-            }
-        });
+        HeroPagerAdapter adapter = new HeroPagerAdapter(this);
+        adapter.add("Test 1", DummyFragment.class, null);
+        adapter.add("Test 2", DummyFragment.class, null);
+        adapter.add("Test 3", DummyFragment.class, null);
+        adapter.add("Test 4", DummyFragment.class, null);
+        adapter.add("Test 5", DummyFragment.class, null);
+        adapter.add("Test 6", DummyFragment.class, null);
+        adapter.add("Test 7", DummyFragment.class, null);
+        adapter.add("Test 8", DummyFragment.class, null);
+        setAdapter(adapter);
+
+        setHeroBackgroundDrawable(getResources().getDrawable(R.drawable.carnarvon_castle));
+
+        setMovingIconDrawable(getResources().getDrawable(R.drawable.myriad_shield));
     }
 
     @Override
@@ -65,6 +67,10 @@ public class MainActivity extends Activity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
         if (id == R.id.action_settings) {
             return true;
         }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 Hero ViewPager
 ==============
+
+Usage
+-----
+
+1. Import the library into your App
+2. Create an Activity subclassing `HeroViewPagerActivity`
+3. Make sure the Activity is using  `Theme.HeroViewPager` or a variant of it.
+4. Supply a `HeroPagerAdapter` to `setAdapter()`
+5. Use `getHeroContentContainer()` to fill the Hero header with content
+6. Run and Test!
